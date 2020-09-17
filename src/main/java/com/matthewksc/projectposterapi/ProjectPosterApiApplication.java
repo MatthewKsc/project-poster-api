83e37aa9aab9dbd210842fb0a51c665fc8dd25b9
MatthewKsc <ksciukmateusz@gmail.com>
2020-09-17 10:22:36 +0200

    Initial commit

---
+++ src/main/java/com/matthewksc/projectposterapi/ProjectPosterApiApplication.java
@@ -0,0 +1,13 @@
+package com.matthewksc.projectposterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPosterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPosterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthewksc/projectposterapi/ProjectPosterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewksc.projectposterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPosterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
