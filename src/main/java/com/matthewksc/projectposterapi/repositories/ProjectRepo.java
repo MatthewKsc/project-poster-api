84ab1a77c43792b5929045b14445be9d52b46758
MatthewKsc <ksciukmateusz@gmail.com>
2020-09-18 13:08:57 +0200

    Simple methods to seek for certain project

--- src/main/java/com/matthewksc/projectposterapi/repositories/ProjectRepo.java
+++ src/main/java/com/matthewksc/projectposterapi/repositories/ProjectRepo.java
@@ -1,8 +1,20 @@
 package com.matthewksc.projectposterapi.repositories;
 
 import com.matthewksc.projectposterapi.entity.Project;
+import com.matthewksc.projectposterapi.entity.enums.City;
+import com.matthewksc.projectposterapi.entity.enums.DeveloperType;
+import com.matthewksc.projectposterapi.entity.enums.LevelOfExperience;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepo extends JpaRepository<Project, Long> {
+
+    List<Project> findAllByCity(City city);
+
+    List<Project> findAllByDeveloperType(DeveloperType developerType);
+
+    List<Project> findAllByRequiredExperience(LevelOfExperience levelOfExperience);
+
 
 }
