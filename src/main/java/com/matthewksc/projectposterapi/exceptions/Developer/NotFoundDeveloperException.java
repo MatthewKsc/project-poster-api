ac141bf011df7890884d96cecabae2da66427845
MatthewKsc <ksciukmateusz@gmail.com>
2020-09-22 13:14:30 +0200

    NotFoundDeveloperAdvice and NotFoundDeveloperException classes implement

---
+++ src/main/java/com/matthewksc/projectposterapi/exceptions/Developer/NotFoundDeveloperException.java
@@ -0,0 +1,12 @@
+package com.matthewksc.projectposterapi.exceptions.Developer;
+
+public class NotFoundDeveloperException extends RuntimeException {
+
+    public NotFoundDeveloperException(Long id) {
+        super("No such Developer with id: " +id);
+    }
+
+    public NotFoundDeveloperException() {
+        super("No object of developer were presented");
+    }
+}
