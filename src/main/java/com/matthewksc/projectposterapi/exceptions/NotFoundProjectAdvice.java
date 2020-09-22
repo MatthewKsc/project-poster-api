cbc246a181842513d4030376811c2b25d1c1c70e
MatthewKsc <ksciukmateusz@gmail.com>
2020-09-22 12:48:43 +0200

    NotFoundProjectAdvice class implement

---
+++ src/main/java/com/matthewksc/projectposterapi/exceptions/NotFoundProjectAdvice.java
@@ -0,0 +1,18 @@
+package com.matthewksc.projectposterapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class NotFoundProjectAdvice {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundProjectException.class)
+    public String NotFoundProjectHandler(NotFoundProjectException exception){
+        return exception.getMessage();
+    }
+}
